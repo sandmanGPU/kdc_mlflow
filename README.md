1a38c96436e5155fb80599e69e1bcafdfc5601c1
sandmanGPU <sandman.gpu@gmail.com>
2023-10-09 05:43:44 +0200

    Training set in pipeline

---
+++ README.md
@@ -0,0 +1,12 @@
+# WORKflow
+
+Update config.yaml
+Update secrets.yaml
+Update params.yaml
+Update entity
+update configuration manager in src config
+update components
+update pipeline
+update main,py
+update dvc.yaml
+app
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_02_prepare_based_model import PrepareBaseModelPipeline
+from src.cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 STAGE_NAME = "Data Ingestion stage"
+
 
 try:
     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
@@ -22,3 +24,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f"==========================================")
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj=ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nX==============================X")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
